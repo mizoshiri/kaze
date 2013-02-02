d7a442305601d25b69d5dc19791e60087afc9787
Ayumi Mizoshiri <ayumi@sazae.com.au>
2013-02-02 15:01:35 +1100

    Move from bitbuket to github

---
+++ site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '442d403ab8ff270bcfd33bac8b3b9119efa99f243da31d49d131d6d256ff58b6724d344a09e515e62b3d13f3fec8cd19d6b006b3d605731821e3916f1b3ed841'
